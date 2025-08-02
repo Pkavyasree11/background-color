6976fba9e4558d37b46088cc71481dbe56f80f32
P.kavyasree <ponnakavyasree@gmail.com>
2025-08-02 11:49:26 +0530

    Add files via upload

---
+++ backjava.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("btn");
+const colorText = document.querySelector(".color");
+
+const hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+
+button.addEventListener("click", () => {
+    const hexcolor = generateHexColor();
+    document.body.style.backgroundColor = hexcolor;  
+    colorText.textContent = hexcolor;                
+});
+
+function generateHexColor() {
+    let hexcolor = "#";
+    for ( i = 0; i < 6; i++) {                    
+        hexcolor += hex[generateRandomNumber()];    
+    }
+    return hexcolor;
+}
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
